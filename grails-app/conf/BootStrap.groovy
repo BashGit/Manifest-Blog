a4a0446eeb4015333c896e2c94c57bd3a9d2f00d
Izaak Baltic <Cbaltic7@aol.com>
2016-02-23 13:48:04 -0500

    Entered login capabilities

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,29 @@
+import com.testapp.*;
+
 class BootStrap {
 
     def init = { servletContext ->
+		
+		def adminRole = new Role(authority: 'ROLE_ADMIN', description: 'Blogger\'s Role').save()
+		def userRole = new Role(authority: 'ROLE_USER', description: 'Users of the blog').save()
+		
+		def blogger = new User(username: 'blogger',password: 'blog').save()
+		def blogUser = new User(username: 'user',password: 'password').save()
+		
+		UserRole.create blogger, adminRole
+		UserRole.create blogUser, userRole
+		
+		UserRole.withSession {
+			it.flush()
+			it.clear()
+			
+		assert Role.count() == 2
+		assert User.count() == 2
+		assert UserRole.count() == 2
+			
+		}	
     }
+	
     def destroy = {
     }
 }
