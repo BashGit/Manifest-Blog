e25e6f89d1e36741fdf396064dd552d5e34caa60
Matthew P Smith <smithmatthewphilip@gmail.com>
2016-02-25 21:31:36 -0500

    fix naming conflict between scenarios

--- features/step_definitions/searchability_steps.rb
+++ features/step_definitions/searchability_steps.rb
@@ -1,4 +1,4 @@
-Given(/^I visit my favorite blogger\'s blog$/) do
+Given(/^I visit the blog for my favorite blogger$/) do
 	visit_favorite_blogger
 end
 
@@ -6,6 +6,7 @@ When(/^I choose a blog post$/) do
 	select_favorite_blog_post
 end
 
+@wip
 Then(/^the url should contain information about the post$/) do
 	blog_post_url = get_blog_post_url
 	expect(blog_post_url).to eq 'http://localhost:8080/blog/blog-entry/show/131'
