4ee1bb0e9e2d83583d8ae765f1091e0d06453c36
Matthew P Smith <smithmatthewphilip@gmail.com>
2016-02-29 12:06:51 -0500

    add comments to bottom of blog entry

--- features/step_definitions/blog_comments_steps.rb
+++ features/step_definitions/blog_comments_steps.rb
@@ -1,7 +1,16 @@
-#Given is in the searchability_steps file
+#Given I visit the blog for my favorite blogger is in the searchability_steps file
 
-#When is in the searchability_steps file
+#When I choose a blog post is in the searchability_steps file
 
-Then (/^my genius comment is at the top of the blog post comments$/) do
+Then (/^I should see comments left by other readers$/) do
 	
+end
+
+Given (^I am reading a blog post from my favorite blogger$) do
+end
+
+When (^I add my genius comment to the blog post$) do
+end
+
+Then (^my genius comment is at the top of the blog post comments$) do
 end
