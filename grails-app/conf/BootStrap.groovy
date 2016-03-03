cc3f52814b66179a1ac646287916af46d3309493
Izaak Baltic <Cbaltic7@aol.com>
2016-03-03 10:57:10 -0500

    Nowww the authentication is needed for certain actions.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,7 +5,7 @@ class BootStrap {
     def init = { servletContext ->
 		
 		def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN', description: 'Blogger\'s Role').save(failOnError: true)
-		def userRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_USER', description: 'Users of the blog').save(failOnError: true)
+		def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER', description: 'Users of the blog').save(failOnError: true)
 		
 		def blogger = User.findByUsername('blogger') ?: new User(username: 'blogger', password: 'blog', enabled: true).save(failOnError: true)
 		
@@ -15,7 +15,7 @@ class BootStrap {
 		
 		def blogUser = User.findByUsername('user') ?: new User(username: 'user', password: 'password', enabled: true).save(failOnError: true)
 		
-		if(!blogUser.authoriteis.contains(userRole)) {
+		if(!blogUser.authorities.contains(userRole)) {
 			UserRole.create blogUser, userRole
 		}
 		
